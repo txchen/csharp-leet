be6250fc7628e3a712bf747c0deb441e4f01ea9a
Tianxiang Chen <txchendev@gmail.com>
2015-11-02 20:53:16 -0800

    update Q219

--- solutions/Q219.cs
+++ solutions/Q219.cs
@@ -1,10 +1,15 @@
 public class Solution {
     public bool ContainsNearbyDuplicate(int[] nums, int k) {
-        for (int i = 0; i < nums.Length; i++) {
-            for (int j = i + 1; j < nums.Length && (j - i) <= k; j++) {
-                if (nums[j] == nums[i]) {
-                    return true;
-                }
+        HashSet<int> hs = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!hs.Add(nums[i]))
+            {
+                return true;
+            }
+            if (i >= k)
+            {
+                hs.Remove(nums[i - k]);
             }
         }
         return false;
